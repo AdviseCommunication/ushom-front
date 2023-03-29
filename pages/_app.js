c1538e59dcd44af437563f30ff8a69f85f710729
Nazmi Aydogdu <nazmi@grapheek.com>
2023-03-29 17:45:30 +0200

    test matomo

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,17 @@ import {useEffect} from 'react'
 import '../src/styles/globals.css'
 import * as gtag from "../src/utils/gtag"
 
+import { init } from "@socialgouv/matomo-next"
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
 const MyApp = ({ Component, pageProps }) => {
     const router = useRouter()
+
+    useEffect(() => {
+        init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+    }, []);
 
     useEffect(() => {
         const handleRouteChange = (url) => {
